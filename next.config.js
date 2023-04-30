0561a3747999390378caeb3b7bee781efa29fcdc
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-04-30 11:20:45 +0900

    add env

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     TMDB_API_KEY: process.env.TMDB_API_KEY,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_NEXTAUTH_SECRET,
   },
   images: {
     domains: ['image.tmdb.org'],
