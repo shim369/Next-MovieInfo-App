b66d1670f3f30cef1d7335cbe73773baa09b2201
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-04-30 11:47:20 +0900

    add secret

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,10 +1,11 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, NEXT_PUBLIC_NEXTAUTH_SECRET } = process.env;
 
 if (!GOOGLE_CLIENT_ID) throw new Error('You must provide GOOGLE_CLIENT_ID env var.');
 if (!GOOGLE_CLIENT_SECRET) throw new Error('You must provide GOOGLE_CLIENT_SECRET env var.');
+if (!NEXT_PUBLIC_NEXTAUTH_SECRET) throw new Error('You must provide NEXT_PUBLIC_NEXTAUTH_SECRET env var.');
 
 export default NextAuth({
   providers: [
@@ -13,4 +14,5 @@ export default NextAuth({
       clientSecret: GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
 });
